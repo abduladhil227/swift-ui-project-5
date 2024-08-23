80824e5edf14942940ba7c1187ec44158406e8d5
abduladhil227 <abduladhil006@gmail.com>
2024-08-23 23:12:57 +0530

    loading resources from app bundle

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -17,6 +17,18 @@ struct ContentView: View {
         }
         .padding()
     }
+    //t’s common to want to look in a bundle for a file you placed there. This uses a new data type called URL
+    //URLs are a bit more powerful than just storing web addresses – they can also store the locations of files, which is why they are useful here.
+    //If we want to read the URL for a file in our main app bundle, we use Bundle.main.url()
+    func testBundles(){
+        
+        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt"){
+            
+            if let fileContent = try? String(contentsOf: fileURL){
+                //
+            }
+        }
+    }
 }
 
 #Preview {
